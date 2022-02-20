e53ca92e00ed7659983f2e76b7279f83f06d43ac
Giri Putra Adhittana <giri.adhittana@binus.edu>
2022-02-20 20:36:34 +0700

    Add Calculateion Function

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/giriputraadhittana01/go-test
+module github.com/giriputraadhittana01/go-test/v2
 
 go 1.16
--- helper/hello.go
+++ helper/hello.go
@@ -6,6 +6,24 @@ func SayHello(name string) {
 	fmt.Println("Hello "+name)
 }
 
+func Calculate(x,y int, operator string) int{
+	switch operator{
+		case "+":
+			return x+y
+		case "-":
+			return x-y
+		case "/":
+			return x/y
+		case "*":
+			return x*y
+		case "%":
+			return x%y
+		default:
+			return -1
+	}  
+}
+
+
 // Awal huruf besar menandakan public
 var Name = "Boba"
 // Awal huruf kecil menandakan private
